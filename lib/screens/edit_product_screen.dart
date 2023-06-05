2e44aaf703fdd85c7e8d01f75bcafd40add87a94
ZAIN ALI <97796695+mrZAIN-ALI@users.noreply.github.com>
2023-06-05 23:19:00 +0500

    implementing url funcationality in admin view

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -10,6 +10,40 @@ class EditPrudcutScreen extends StatefulWidget {
 class _EditPrudcutScreenState extends State<EditPrudcutScreen> {
   final _priceFocusNode = FocusNode();
   final _descriptionNOde = FocusNode();
+  final _imageUrlController = TextEditingController();
+  final _ImageUrlFocuNode=FocusNode();
+
+  
+  String _imgUrl = "";
+  void dispose() {
+    _ImageUrlFocuNode.removeListener(_updatImageurlFun);
+    _priceFocusNode.dispose();
+    _descriptionNOde.dispose();
+    _imageUrlController.dispose();
+    _ImageUrlFocuNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _ImageUrlFocuNode.addListener(_updatImageurlFun);
+    super.initState();
+  }
+  void _updatImageurlFun(){
+    if(!_ImageUrlFocuNode.hasFocus){
+      setState(() {
+        
+      });
+    }
+  }
+  // void updateImageUrl() {
+  //   if (!_imageUrlController.text.isEmpty) {
+  //     setState(() {
+  //       _imgUrl = _imageUrlController.text;
+  //     });
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -49,6 +83,50 @@ class _EditPrudcutScreenState extends State<EditPrudcutScreen> {
                 keyboardType: TextInputType.multiline,
                 focusNode: _descriptionNOde,
                 maxLines: 3,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Container(
+                    height: 100,
+                    width: 100,
+                    margin: EdgeInsets.only(top: 10, left: 10, right: 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                          width: 1,
+                          color: Theme.of(context).colorScheme.primary),
+                    ),
+                    child: Container(
+                      child: _imageUrlController.text.isEmpty
+                          ? const Center(
+                              child: Text(
+                                "Enter Url to load image",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                ),
+                                maxLines: 2,
+                                textAlign: TextAlign.center,
+                              ),
+                            )
+                          : FittedBox(
+                              child: Image.network(
+                                 _imageUrlController.text,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                    ),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      keyboardType: TextInputType.url,
+                      decoration: const InputDecoration(
+                        labelText: "Image Url",
+                      ),
+                      controller: _imageUrlController,
+                      focusNode: _ImageUrlFocuNode,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
